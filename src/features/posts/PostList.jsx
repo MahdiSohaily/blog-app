a69e0d69dbb2c16cbe9edb9b3dc13a93560bbd67
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-26 11:06:55 +0430

    fetch posts in the post list component

--- src/features/posts/PostList.jsx
+++ src/features/posts/PostList.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Post from './Post';
-import { selectPostIds } from './postsSlice';
+import { fetchPosts, selectPostIds } from './postsSlice';
 
 export default function PostList() {
   const postIds = useSelector(selectPostIds);
   const content = postIds.map((id) => <Post key={id} id={id} />);
+  const status = useSelector((state) => state.posts.status);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if ('ideal' === status) {
+      dispatch(fetchPosts());
+    }
+  }, [dispatch, status]);
   return (
     <section className="posts-list">
       <h2>Posts</h2>
