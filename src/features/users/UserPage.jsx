b81e421793eb0fbfa14bfd37b54c008b36cc0786
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-26 23:08:26 +0430

    Add userPage component

---
+++ src/features/users/UserPage.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function UserPage() {
+  return (
+    <section>
+      <h2>Candace Braun</h2>
+      <ul>
+        <li>
+          <a href="/posts/Sq0OZJyMWGrjkJAX7BzU-" dideo-checked="true">
+            Those pandas are nothing more than sheeps?
+          </a>
+        </li>
+        <li>
+          <a href="/posts/s1n1EaGGX8YKIm9LN_GB5" dideo-checked="true">
+            The impartial shark reveals itself as an amused frog to those who
+            look.
+          </a>
+        </li>
+        <li>
+          <a href="/posts/3VWHc4H5lTvB9xAU0iDZ9" dideo-checked="true">
+            The zeitgeist contends that some posit the witty scorpion to be less
+            than successful?
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}
