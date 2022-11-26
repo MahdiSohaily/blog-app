92045aa57a4c292672e4da58d7badc258943db6d
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-26 09:15:28 +0430

    Add users reducer tot the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
-import postsSlice from "./posts/postsSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import postsSlice from './posts/postsSlice';
+import usersSlice from './users/usersSlice';
 
 const store = configureStore({
-    reducer: {
-        posts: postsSlice,
-    }
-})
+  reducer: {
+    posts: postsSlice,
+    users: usersSlice,
+  },
+});
 
 export default store;
