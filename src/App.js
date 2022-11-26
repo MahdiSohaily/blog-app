f85598412851c72bb7cb190768d31b7ec1c5db3c
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-26 23:03:05 +0430

    Update project routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './app/Navbar';
 import AddPost from './features/posts/AddPost';
 import PostList from './features/posts/PostList';
+import UsersList from './features/users/UsersList';
 function App() {
   return (
     <>
       <Router>
         <Navbar />
         <div className="App">
-          <AddPost />
-          <PostList />
+          <switch>
+            <Route path="/" exact>
+              <AddPost />
+              <PostList />
+            </Route>
+            <Route path="/users" exact>
+              <UsersList />
+            </Route>
+          </switch>
         </div>
       </Router>
     </>
