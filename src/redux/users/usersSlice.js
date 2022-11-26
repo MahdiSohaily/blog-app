eb46e8f0bf2c8d7147ee2eb386e848bad4af1116
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-26 09:15:03 +0430

    Add users slice script

---
+++ src/redux/users/usersSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState: {},
+  reducers: {},
+  extraReducers: {},
+});
+
+export default usersSlice.reducer
